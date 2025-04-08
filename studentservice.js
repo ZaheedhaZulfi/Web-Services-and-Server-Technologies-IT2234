ab77667dd5de42f063e4319b93a55198b71e30f6
ZaheedhaZulfi <fathimazaheedhazulfi@gmail.com>
2025-04-09 02:29:24 +0530

    Add files via upload

---
+++ studentservice.js
@@ -0,0 +1,15 @@
+const students = require('./studentdb')
+
+function getstudents() {
+    return students;
+}
+
+function getstudent(id) {
+    return students.find((student)=>student.regno==id)
+}
+
+function getByGender(gender) {
+    return students.filter((student)=>student.gender==gender)
+}
+
+module.exports={getstudent,getstudents,getByGender}
